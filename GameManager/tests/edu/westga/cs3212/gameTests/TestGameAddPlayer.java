4e409a0dad18d5cc5fe3ca53a43edbf36126dbcd
bmonue1 <bmonue1@my.westga.edu>
2018-02-18 19:43:19 -0500

    Added Junit Test Game: AddPlayer

---
+++ GameManager/tests/edu/westga/cs3212/gameTests/TestGameAddPlayer.java
@@ -0,0 +1,43 @@
+package edu.westga.cs3212.gameTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.gamemanager.model.Game;
+import edu.westga.cs3212.gamemanager.model.Player;
+
+class TestGameAddPlayer {
+	
+	@Test
+	void testAddNullPlayers() {
+		Game game = new Game("game1");
+		Throwable exception = assertThrows(IllegalArgumentException.class, () ->{
+			game.addPlayer(null);
+		});
+		assertEquals("Player must exist to be added", exception.getMessage());
+	}
+
+	@Test
+	void testAddPlayer() {
+		Game game = new Game("game1");
+		Player p1 = new Player("Tim", 50);
+		game.addPlayer(p1);
+		assertEquals(1, game.getNumberOfPlayers());
+	}
+	
+	@Test
+	void testAddMultiPlayers() {
+		Game game = new Game("game1");
+		Player p1 = new Player("Tim", 50);
+		Player p2 = new Player("Jack", 40);
+		Player p3 = new Player("Doug", 30);
+		
+		game.addPlayer(p1);
+		game.addPlayer(p2);
+		game.addPlayer(p3);
+		
+		assertEquals(3, game.getNumberOfPlayers());
+	}
+
+}

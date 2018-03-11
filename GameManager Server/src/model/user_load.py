29e7797c9f52ae85c98d2e3156ab8bb0a91cc90e
bmonue1 <bmonue1@my.westga.edu>
2018-03-11 17:26:13 -0400

    Edit user constructer in py + added tests for user load

--- GameManager Server/src/model/user_load.py
+++ GameManager Server/src/model/user_load.py
@@ -4,46 +4,47 @@ Created on Mar 1, 2018
 @author: Bryan, Dallas, Josh, Caleb
 @version: spring 2018
 '''
-class UserManager:
+class User:
     '''TODO
     username: if '''
-    def _init_(self, username, completedGames = None, inProgress = None, currentGame = None) :
+    def _init_(self, username) :
         if(username == None):
             raise ValueError("username cannot be none")
+        if(username == ""):
+            raise ValueError("username cannot be empty")
+        
         self._username = username
-        if(completedGames == None):
-            self._completedGames = []
-        else:
-            self._completedGames= completedGames
-        if(inProgress == None):
-            self._inProgress = []
-        else:
-            self._inProgress= inProgress
-        if(currentGame == None):
-            self._currentGame = None
-        else:
-            self._currentGame= currentGame
-            
+        self._completedGames = []
+        self._inProgress = []
+        self._currentGame = Game("Default Game Name")
+        
     def getUsername(self):
         return self._username
+    
     def getCompletedGames(self):
         return self._completedGames
+   
     def getInProgressGames(self):
         return self._inProgress
+    
     def getCurrentGame(self):
         return self._currentGame
+    
     def setCurrentGame(self, currGame):
         if (currGame == None):
             raise ValueError("Current game must exist")
         self._currentGame = currGame
+    
     def setInProgressGames(self, games):
         self._inProgress = games
+    
     def setUsername(self, username):
         if(username == None):
             raise ValueError("Invalid username")
         if(username == ""):
             raise ValueError("Username cannot be empty")
         self._username = username
+    
     def addCompletedGame(self, game):
         if(game == None):
             raise ValueError("Game must exist")

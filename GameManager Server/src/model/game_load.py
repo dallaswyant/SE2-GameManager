e4a29cab7452158feb04ebcd7d4e89153844b5e8
Caleb <cbell12@my.westga.edu>
2018-03-26 18:22:53 -0400

    Added specs for game_load

--- GameManager Server/src/model/game_load.py
+++ GameManager Server/src/model/game_load.py
@@ -26,35 +26,88 @@ class Game:
         self._name = name
         self._isCompleted = False
     
+    '''
+    adds player
+    @precondition: player != none player must be player
+    @postcondition: player is added
+    '''
+
     def addPlayer(self, player):
         if(player == None):
             raise ValueError("Player must exist")
         if(not isinstance(player, Player)):
             raise ValueError("insertion must be a Player object")
         self._players.append(player)
-        
+       
+    '''
+    removes player
+    @precondition: player != none player must be player
+    @postcondition: player is removed
+    ''' 
+
     def removePlayer(self, player):
         if(not isinstance(player, Player)):
             raise ValueError("insertion must be a Player object")
         if player not in self._players:
             raise ValueError("List must contain the player")
         self._players.remove(player)
-        
+
+    '''
+    returns number of players
+    @postcondition: none
+    @postcondition: none
+    @return: number of players
+    '''    
+
     def getNumberOfPlayers(self):
         return len(self._players)
     
+    '''
+    returns completed status
+    @postcondition: none
+    @postcondition: none
+    @return: status
+    '''  
+
     def getCompletedStatus(self):
         return self._isCompleted
     
+    '''
+    sets completed status
+    @postcondition: status != none
+    @postcondition: status 
+    ''' 
+
     def setCompleteStatus(self, status):
         self._isCompleted = status
-        
+      
+    '''
+    returns string description of class
+    @postcondition: none
+    @postcondition: none
+    @return: toString
+    '''  
+
     def toString(self):
         return self._name
     
+    '''
+    returns players
+    @postcondition: none
+    @postcondition: none
+    @return: players
+    '''
+
     def getPlayers(self):
         return self._players
     
+    '''
+    returns points increment
+    @postcondition: none
+    @postcondition: none
+    @return: point increment value
+    '''
+
     def getPointsIncrementValue(self):
         return self._pointsIncrementValue
          

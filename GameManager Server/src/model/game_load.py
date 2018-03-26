4006f7228c29e364b2e0d9d20c589aa10616ba1a
bmonue1 <bmonue1@my.westga.edu>
2018-03-26 12:57:08 -0400

    Enforced player object in game py condition

--- GameManager Server/src/model/game_load.py
+++ GameManager Server/src/model/game_load.py
@@ -3,12 +3,14 @@ Created on Mar 5, 2018
 
 @author: Bryan, Caleb, Dallas, Josh
 '''
-
+from model.player_load import Player
 class Game:
     def __init__(self, name, pointsIncrementValue):
         if(name == None):
             raise ValueError("Name cannot be None")
-        if(name == ""):
+        if (not isinstance(name, str)):
+            raise ValueError("Name value must be string")
+        if (name == ""):
             raise ValueError("Name cannot be empty")
         if(pointsIncrementValue == None):
             self._pointsIncrementValue = 1
@@ -25,9 +27,13 @@ class Game:
     def addPlayer(self, player):
         if(player == None):
             raise ValueError("Player must exist")
+        if(not isinstance(player, Player )):
+            raise ValueError("insertion must be a Player object")
         self._players.append(player)
         
     def removePlayer(self, player):
+        if(not isinstance(player, Player )):
+            raise ValueError("insertion must be a Player object")
         if player not in self._players:
             raise ValueError("List must contain the player")
         self._players.remove(player)

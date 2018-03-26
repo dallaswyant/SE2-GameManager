d1a4070d5eb42e73a91a85fa847fa2bb378628db
Caleb <cbell12@my.westga.edu>
2018-03-26 18:41:05 -0400

    added specs for user_load

--- GameManager Server/src/model/user_load.py
+++ GameManager Server/src/model/user_load.py
@@ -26,6 +26,13 @@ class User:
         self._currentGame = Game("Default Game Name", 1)
         self._playerId = self._getNextId()
     
+    '''
+    adds completed game
+    @precondition: game != None game == game
+    @postcondition: game is completed
+    @param game: game to complete 
+    '''
+        
     def addCompletedGame(self, game):
         if(game == None):
             raise ValueError("Game must exist")
@@ -38,22 +45,33 @@ class User:
         self._completedGames.append(game)
 
     '''
-    Determines the next id to be used
+    returns next id
+    @precondition: none
+    @postcondition: none
+    @return: next id
     '''
 
     def _getNextId(self):
         User._idCounter += 1
         return User._idCounter
-    
+
     '''
-    Get the id for the player
-    
-    @return the id for the player
+    returns player id
+    @precondition: none
+    @postcondition: none
+    @return: player id
     '''
 
     def getPlayerId(self):
         return self._playerId
     
+    '''
+    adds in progress game
+    @precondition: game != None game == game
+    @postcondition: game is completed
+    @param game: game to added to in progress 
+    '''
+
     def addInProgressGame(self, game):
         if(game == None):
             raise ValueError("Game must exist")
@@ -65,15 +83,43 @@ class User:
             self._inProgress.remove(game)
         self._inProgress.append(game)
     
+    '''
+    returns completed games
+    @precondition: none
+    @postcondition: none
+    @return: completed games
+    '''
+
     def getCompletedGames(self):
         return self._completedGames
     
+    '''
+    returns in progress games
+    @precondition: none
+    @postcondition: none
+    @return: in progress games
+    '''
+
     def getInProgressGames(self):
         return self._inProgress
     
+    '''
+    returns user name
+    @precondition: none
+    @postcondition: none
+    @return: user name
+    '''
+
     def getUsername(self):
         return self._username
     
+    '''
+    sets user name
+    @precondition: username != None username == string username != "" 
+    @postcondition: user name is set
+    @param username: user name to be set
+    '''
+
     def setUsername(self, username):
         if(username == None):
             raise ValueError("Invalid user name")
@@ -83,9 +129,23 @@ class User:
             raise ValueError("User name cannot be empty")
         self._username = username
     
+    '''
+    returns completed game
+    @precondition: none
+    @postcondition: none
+    @return: completed game
+    '''
+
     def getCurrentGame(self):
         return self._currentGame
     
+    '''
+    sets current game
+    @precondition: currGame != None currGame == game
+    @postcondition: currentGame is set
+    @param currGame: current game to be set
+    '''
+
     def setCurrentGame(self, currGame):
         if (currGame == None):
             raise ValueError("Current game must exist")
@@ -93,9 +153,15 @@ class User:
             raise ValueError("Must be a Game object")
         self._currentGame = currGame
     
+    '''
+    sets in progress games
+    @precondition: games cannot be empty list games must be list of games
+    @postcondition: inProgress is set
+    @param games: in progress games list
+    '''
+
     def setInProgressGames(self, games):
         if (not isinstance(games, list)):
             raise ValueError("Must be a list of games")
         self._inProgress = games
-    
         
